8f50d02d35d9e261f0afe16db81ca8b0eb835174
Zarek Jenkinson <me@akiwiguy.net>
2012-11-07 23:15:01 +1300

    More commands in imouto.py

--- xbot/modules/imouto.py
+++ xbot/modules/imouto.py
@@ -59,3 +59,66 @@ def hug(bot, args):
 
 util.register(hug, "common", "hug")
 
+def slap(bot, args):
+    botresponses = [
+        ";__;",
+        "W-what was that for?",
+        "Of course I'll slap you, %s~" % bot.remote['nick'],
+        "\x01ACTION slaps %s\x01" % bot.remote['nick'],
+        "\x01ACTION gets slapped by %s\x01" % bot.remote['nick']
+    ]
+
+    if len(args) == 2:
+        nicks = bot.inv['rooms'].get(bot.remote['receiver'])
+        if nicks:
+            if args[1].lower() in [nick.lower() for nick in nicks]:
+                if args[1].lower() != bot.name.lower():
+                    responses = [
+                        "%s slaps %s with the force of a thousand flaming suns of fury" % (bot.remote['nick'], args[1]),
+                        "%s slaps %s" % (bot.remote['nick'], args[1]),
+                        "%s doesn't slap %s" % (bot.remote['nick'], args[1]),
+                        "%s slaps %s" % (args[1], bot.remote['nick'])
+                    ]
+                    util.answer(bot, __import__('random').choice(responses))
+                else:
+                    util.answer(bot, __import__('random').choice(botresponses))
+            else:
+                util.answer(bot, "B-but %s isn't here!" % args[1])
+        else:
+            util.answer(bot, "B-baka!")
+    else:
+        util.answer(bot, __import__('random').choice(botresponses))
+
+util.register(slap, "common", "slap")
+
+def kiss(bot, args):
+    botresponses = [
+        "\x01ACTION kisses %s on the cheek\x01" % bot.remote['nick'],
+        "\x01ACTION kisses %s\x01" % bot.remote['nick'],
+        "\x01ACTION kisses %s's foot\x01" % bot.remote['nick'],
+        "L-lewd!",
+        "W-why would I want to do that?"
+    ]
+
+    if len(args) == 2:
+        nicks = bot.inv['rooms'].get(bot.remote['receiver'])
+        if nicks:
+            if args[1].lower() in [nick.lower() for nick in nicks]:
+                if args[1].lower() != bot.name.lower():
+                    responses = [
+                        "%s kisses %s on the cheek" % (bot.remote['nick'], args[1]),
+                        "%s kisses %s in the lewdest way possible" % (bot.remote['nick'], args[1]),
+                        "%s kisses %s's foot" % (bot.remote['nick'], args[1]),
+                        "%s sneakily kisses %s on the lips before running away~~~" % (bot.remote['nick'], args[1])
+                    ]
+                    util.answer(bot, __import__('random').choice(responses))
+                else:
+                    util.answer(bot, __import__('random').choice(botresponses))
+            else:
+                util.answer(bot, "B-but %s isn't here!" % args[1])
+        else:
+            util.answer(bot, "B-baka!")
+    else:
+        util.answer(bot, __import__('random').choice(botresponses))
+
+util.register(kiss, "common", "kiss")
